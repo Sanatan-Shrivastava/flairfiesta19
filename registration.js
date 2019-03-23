8072cbb34443b225ac53744685ae22cc0aae6d0d
Sanatan Shrivastava <sanatan.cseiiitk@gmail.com>
2019-03-23 17:47:40 +0530

    development

--- registration.js
+++ registration.js
@@ -1,251 +1,199 @@
-const user = require("./models").user
-const router = require('express').Router()
-const sgMail = require('@sendgrid/mail');
-// const events = require('../events.json')
-// const workshops = require('../workshops.json')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+/* adding key in questions array, also make sure that position of collegeid remains 5*/
+    var questions = [
+    {question:"What's your first name?", type: "text", key: "firstname"},
+    {question:"What's your last name?", type: "text", key: "lastname"},
+    {question:"What's your college?", type: "text", key: "college"},
+    {question:"What's your college ID?", type: "text", key: "college_id"},
+    {question:"What's your email?", pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ , key: "email"},
+    {question:"What's your mobile?", pattern: /^\d{10}$/, key: "phone"},
+    {question:"Create your PIN (4 Digits)", type: "password", pattern: /^[1-9]\d{3}$/ , key: "pin"},
+    {question:"Are you a Student of IIITK?(y/n)", type: "text", pattern: /^y|n$/i , key: "iiitkian"}
+  ];
 
-router.post('/', async (req, res) => {
-    let data = req.body
-    if (data.honeypot) {
-        return res.status(401).json({ok:false, data: {err: "Suspicious behaviour"}})
-    }
-    if (!data.firstName || !data.lastName) {
-        return res.status(400).json({ok:false, data: {err: "Full name is necessary"}})
-    } else {
-        data.firstName = data.firstName.trim().toLowerCase()
-        data.lastName = data.lastName.trim().toLowerCase()
-    }
-    if (!data.email) {
-        return res.status(400).json({ok:false, data: {err: "Email is necessary"}})
-    } else {
-        data.email = data.email.trim().toLowerCase()
-    }
-    if(!data.college || !data.collegeID) {
-        return res.status(400).json({ok:false, data: {err: "College details are necessary"}})
-    } else {
-        data.college = data.college.trim().toLowerCase()
-        data.collegeID = data.collegeID.trim().toLowerCase()
-    }
-    if (!data.PIN) {
-        return res.status(400).json({ok: false, data: {err: "PIN is necessary"}})
-    } else {
-        data.PIN = Number(data.PIN)
-        if(!(data.PIN/1000)) {
-            return res.status(400).json({ok:false, data: {err: "Incorrect PIN format"}})
-        }
-    }
-    if(!data.mob) {
-        return res.status(400).json({ok:false, data: {err: "Mobile number is required"}})
-    } else {
-        data.mob = Number(data.mob)
-        if (!(data.mob / 10000000000)) {
-            return res.status(400).json({ok:false, data: {err: "Enter correct mobile number"}})
-        }
-    }
+  var college_id = "" ;
+  var alreadyRegistered = false ;
 
-    let exists  = await user.find({
-        "collegeID": data.collegeID
-    })
-    if(exists.length != 0) {
-        return res.status(401).json({ok:false, data:{err: "You have already registered", isRegistered: true}})
-    }
-    user.create({
-        firstName: data.firstName,
-        lastName: data.lastName,
-        email: data.email,
-        college: data.college,
-        collegeID: data.collegeID,
-        mob: data.mob,
-        flairPIN: data.PIN,
-        isIIITK: data.isIIITK,
-        events: {},
-        flairID: (await getID().next()).value
-    })
-    .then(result => {
-        if(process.env.NODE_ENV === 'production' || process.env.SEND_MAIL) {
-            sendRegistrationEmail(result.email, titleCase(`${result.firstName} ${result.lastName}`), result.flairID, result.flairPIN)
-        }
-        return res.status(200).json({ok:true, data:result})
-    })
-    .catch(err => {
-        console.error(`MONGO ERROR: while saving user ${err}`)
-    })
-})
+/**********
 
-router.post('/events', async (req, res) => {
-   let {flairID, FlairPIN, society, teamName, _event, teamSize} = req.body
-    if(!flairID || !_event || !flairPIN || !society ) {
-        return res.status(401).json({ok: false, data: {err:"Incorrect details"}})
-    }
-    if(!("" + blitzID).match(/blitz@\d{4,}/))
-    if(!(society in events)) {
-        return res.status(404).json({ok: false, data: {err: "Incorrect society"}})
-    }
-    if(!(_event in events[society])) {
-        return res.status(404).json({ok:false, data: {err: "Incorrect event"}})
-    }
-    if(events[society][_event].isTeam && !(teamName && teamSize)) {
-        return res.status(401).json({ok: false, data: {err:"Team name and size is necessary for team events"}})        
-    }
-    flairID = flairID.split("@")[1]
-    let old = {}
-    try {
-        let result  = await user.find({
-            flairID
-        })
-        if(result.length === 0) {
-            return res.status(400).json({ok:true,data:{err:"Incorrect FlairID"}})
-        }
-        if (result[0].flairPIN != flairPIN) {
-            return res.status(401).json({ok: false, data: {err: "Incorrect FlairPIN"}})
-        } else {
-            old = result[0]
-        }
-    } catch(err) {
-        console.log(`MONGO ERROR: in /events .find ${err}`)
-    }
-    if (!old.events) {
-        old.events = {}
-    }
+  !!!!!
+  New Version: https://codepen.io/arcs/pen/rYXrNQ
+  !!!!!
+  
+  Credits for the design go to XavierCoulombeM
+  https://dribbble.com/shots/2510592-Simple-register-form
+  
+  This Pen uses no libraries except fonts and should 
+  work on all modern browsers
+  
+  The answers are stored in the `questions` array
+  with the key `value`. 
 
-    if (!old.events[club]) {
-        old.events[club] = []
-    }
+ **********/
 
-    if(old.events[club].some(elem => elem.name === _event)) {
-        return res.status(401).json({ok: false, data: {err: "You have already registered"}})
-    }
+(function(){
 
-    let newEvent = {name: _event}
-    if (teamName && teamSize) {
-        newEvent.teamName = teamName
-        newEvent.teamSize = teamSize
-    }
-    old.events[club].push(newEvent)
+  var tTime = 100  // transition transform time from #register in ms
+  var wTime = 200  // transition width time from #register in ms
+  var eTime = 1000 // transition width time from inputLabel in ms
 
-    user.findOneAndUpdate({
-        flairID
-    }, old, {
-        new: true
-    })
-    .then((result) => {
-        if(process.env.NODE_ENV === 'production' || process.env.SEND_MAIL) {
-            sendEventEmail(result.email, "" + result.firstName + result.lastName,_event,
-            result.teamName, result.teamSize)
-        }
-        return res.status(200).json({ok: true, data: {updatedDoc: result}})
-    })
-    .catch(err => {
-        console.error(`[MONGO ERROR]: ${err.message || ''} stack: ${err.stack}`)
-        return res.status(500).json({ok: false, data:{err: "Internal Error"}})
-    })
-})
+  // init
+  // --------------
+  var position = 0
 
-function sendEventEmail (to, name, eventName, teamName, teamSize) {
-    const msg = {
-        to,
-        from: {
-            email: 'Team@flairFiesta.in',
-            name: "Team FlairFiesta" 
-        },
-        subject: `Thanks for registering in ${eventName}`,
-        text: `Hi ${name},
-        Thanks for registering ${teamName? `team ${teamName}`: ''}  for ${eventName}.
-        looking forward to see ${teamSize? `your team of ${teamSize}` : 'you'} at FlairFiesta.
+  putQuestion()
+
+  progressButton.addEventListener('click', validate)
+  inputField.addEventListener('keyup', function(e){
+    transform(0, 0) // ie hack to redraw
+    if(e.keyCode == 13) validate()
+  })
+
+  // functions
+  // --------------
+
+  // load the next question
+  function putQuestion() {
+    inputLabel.innerHTML = questions[position].question
+    inputField.value = ''
+    inputField.type = questions[position].type || 'text'  
+    inputField.focus()
+    showCurrent()
+  }
+  
+  // when all the questions have been answered
+  
+
+  // when submitting the current question
+  function validate() {
+
+    // set the value of the field into the array
+    questions[position].value = inputField.value
+
+    // check if the pattern matches
+    if (!inputField.value.match(questions[position].pattern || /.+/)) wrong()
+    else ok(function() {
+
+      if(position === 3 ){
+        questions[3].value = questions[3].value.toLowerCase();
+        checkRegistered(questions[position].value);  
+      }
+
+      if(!alreadyRegistered &&  position == 7 ){
+        createIdAndFeedAllData();
+      }else if(alreadyRegistered && position == 7){
+        completed("nothing");
+      }
+
+      // set the progress of the background
+      progress.style.width = ++position * 100 / questions.length + 'vw'
         
-        In the meanwhile checkout other events and workshops @ Blitzschlag.org
+      // if there is a new question, hide current and load next
+      if (questions[position]) hideCurrent(putQuestion)
+      else hideCurrent(done)
+             
+    })
 
-        Welcome to FlairFiesta'19!!
-        Team FlairFiesta`,
-        html: `<p>Hi ${name},</p><br>
-        <p>Thanks for registering ${teamName? `team <strong>${teamName}</strong>` : ''} for ${eventName}.</p><br>
-        <p>We look forward to see ${teamSize? `your team of ${teamSize}`: 'you'} at FlairFiesta.</p><br>
-        <p>In the meanwhile checkout other events and workshops @ <a href="flairfiesta.in">flairfiesta.in</a></p><br>
-        <p><strong> Welcome to FlairFiesta'19!!</strong></p>
-        <p>Team FlairFiesta</p>`
-    }
-    sgMail.send(msg)
-}
+  }
 
-function sendRegistrationEmail (to, name, blitzID, blitzPIN ) {
-    const msg = {
-        to,
-        from: {
-            email: 'Team@flairfiesta.in',
-            name: "Team FlairFiesta" 
-        },
-        subject: 'Thanks for registering for FlairFiesta',
-        text: `Hi ${name},
-        Thanks for registering for FlairFiesta.
-        Your blitzID is ${blitzID}.
-        And your flairPIN is ${leftPadZero(flairPIN)}
+  // helper
+  // --------------
 
-        Welcome to FlairFiesta'19!!
-        Team FlairFIesta
-        `,
-        html: `<p>Hi ${name},</p>
-        <p><br>Thanks for registering for FlairFiesta'19.</p>
-        <p>Your FlairID is <strong>flair@${flairID}</strong>.</p>
-        <p>And your flairPIN is <strong>${leftPadZero(flairPIN)}</strong>.</p>
-        <p><strong>Welcome to FlairFiesta'19!!</strong></p>
-        <p>Team FlairFiesta</p>`
-        `
-      };
-      sgMail.send(msg);
-}
+  function hideCurrent(callback) {
+    inputContainer.style.opacity = 0
+    inputProgress.style.transition = 'none'
+    inputProgress.style.width = 0
+    setTimeout(callback, wTime)
+  }
 
-var _asyncGenerator = function () { function AwaitValue(value) { this.value = value; } function AsyncGenerator(gen) { var front, back; function send(key, arg) { return new Promise(function (resolve, reject) { var request = { key: key, arg: arg, resolve: resolve, reject: reject, next: null }; if (back) { back = back.next = request; } else { front = back = request; resume(key, arg); } }); } function resume(key, arg) { try { var result = gen[key](arg); var value = result.value; if (value instanceof AwaitValue) { Promise.resolve(value.value).then(function (arg) { resume("next", arg); }, function (arg) { resume("throw", arg); }); } else { settle(result.done ? "return" : "normal", result.value); } } catch (err) { settle("throw", err); } } function settle(type, value) { switch (type) { case "return": front.resolve({ value: value, done: true }); break; case "throw": front.reject(value); break; default: front.resolve({ value: value, done: false }); break; } front = front.next; if (front) { resume(front.key, front.arg); } else { back = null; } } this._invoke = send; if (typeof gen.return !== "function") { this.return = undefined; } } if (typeof Symbol === "function" && Symbol.asyncIterator) { AsyncGenerator.prototype[Symbol.asyncIterator] = function () { return this; }; } AsyncGenerator.prototype.next = function (arg) { return this._invoke("next", arg); }; AsyncGenerator.prototype.throw = function (arg) { return this._invoke("throw", arg); }; AsyncGenerator.prototype.return = function (arg) { return this._invoke("return", arg); }; return { wrap: function (fn) { return function () { return new AsyncGenerator(fn.apply(this, arguments)); }; }, await: function (value) { return new AwaitValue(value); } }; }();
+  function showCurrent(callback) {
+    inputContainer.style.opacity = 1
+    inputProgress.style.transition = ''
+    inputProgress.style.width = '100%'
+    setTimeout(callback, wTime)
+  }
 
-let getID = (() => {
-    var _ref = _asyncGenerator.wrap(function* () {
-        let lastRec = (yield _asyncGenerator.await(user.find({}, ['flairID'], {
-            sort: {
-                flairID: -1
-            }
-        })))
-        let lastID = lastRec.length ? lastRec[0].flairID : 3000
-        while (true) {
-            yield lastID + 1;
-        }
-    });
+  function transform(x, y) {
+    register.style.transform = 'translate(' + x + 'px ,  ' + y + 'px)'
+  }
 
-    return function getID() {
-        return _ref.apply(this, arguments);
-    };
-})();
-/** The above is refactored version of the following function transpiled using babel:
-async function* getID() {
-    let lastID = users.find({}, ['flairID'], {
-        sort: {
-           flairID: -1
+  function ok(callback) {
+    register.className = ''
+    setTimeout(transform, tTime * 0, 0, 10)
+    setTimeout(transform, tTime * 1, 0, 0)
+    setTimeout(callback,  tTime * 2)
+  }
+
+  function wrong(callback) {
+    register.className = 'wrong'
+    for(var i = 0; i < 6; i++) // shaking motion
+      setTimeout(transform, tTime * i, (i%2*2-1)*20, 0)
+    setTimeout(transform, tTime * 6, 0, 0)
+    setTimeout(callback,  tTime * 7)
+  }
+
+  //check user is already registered or not
+  function checkRegistered(college_id){
+    var database = firebase.database();
+    database.ref('Users').orderByChild('college_id').equalTo(college_id).once('value').then(function(snapshot) {
+        var x = snapshot.val()
+        if(x == null){
+          this.college_id = college_id ; 
+        }else{
+          alreadyRegistered = true ;
         }
     })
-    while (true) {
-        if (lastID % 20) {
-            yield lastID + Math.floor(50*Math.random() + 120)
-        } else {
-            yield lastID + 1
-        }
+  }
+
+  //create flairId and push data
+  function createIdAndFeedAllData(){
+    var database = firebase.database();
+    
+    database.ref('Users/flairId' ).once('value').then(function(snapshot) {
+        //create flairId
+        var flairId = "flair@";
+        var num = snapshot.val();
+        num = num + 1;
+        var key = num ;
+        flairId = flairId + num.toString();
+        database.ref('Users/flairId').set(num);
+
+        //create object and push in flairId
+        var user = {};
+        var i = 0;
+        for(i = 0;i <= 7 ;i++)
+          user[questions[i].key] = questions[i].value;
+        user["flairId"] = flairId ;
+
+        firebase.database().ref('Users/'+key).set(user);
+
+        //completed
+        completed(flairId);
+    })
+  }
+
+  //call when everything completed
+  function completed(flairId){
+    // remove the box if there is no next question
+    register.className = 'close'
+    
+    // add the h1 at the end with the welcome text
+    var h1 = document.createElement('h1')
+    h1.setAttribute('style', 'white-space: pre;');
+    
+    if(!alreadyRegistered){
+        h1.textContent = "Welcome to FlairFiesta 2019 " + questions[0].value + ' !\n' ;
+        h1.textContent += "Your Flair Id is "+ flairId ;
+    }else{
+      h1.textContent = "You have already registered " + questions[0].value + ' !';
     }
-}
-*/
+    
+    h1.style.textAlign = "center" ;
+    setTimeout(function() {
+      register.parentElement.appendChild(h1)
+      setTimeout(function() {
+        h1.style.opacity = 1
+      }, 50)
+    }, eTime)
+  }
 
-function titleCase(str) {
-    return  str.toLowerCase().split(' ').map(word => 
-        `${word.substring(0,1).toUpperCase()}${word.substring(1)}`)
-    .join(" ");
-}
-
-function leftPadZero (num) {
-    let numStr = "" + num
-    while (true) {
-        if (numStr.length <= 4) {
-            return numStr
-        } else {
-            numStr = "0" + numStr
-        }
-    }
-}
-
-module.exports = router
+}())
